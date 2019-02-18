28813f91e4c2104ccf23f87bbea0582c4538f892
Theary Im and Yulia Shidlovskaya <theary+yulia@>
2019-02-18 13:37:51 -0800

    Add MadLibs, ZooForm and ToyForm to source

---
+++ MadLibs/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using MadLibs.Models;
+
+namespace MadLibs.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/zoo")]
+    public ActionResult Zoo(string adjective1, string noun1, string verb1, string adverb1, string adjective2, string noun2, string noun3, string adjective3, string verb2, string adverb2, string verb3, string adjective4) {
+      ZooVariable myZooVariable = new ZooVariable();
+      myZooVariable.SetNoun1(noun1);
+      myZooVariable.SetNoun2(noun2);
+      myZooVariable.SetNoun3(noun3);
+      myZooVariable.SetAdjective1(adjective1);
+      myZooVariable.SetAdjective2(adjective2);
+      myZooVariable.SetAdjective3(adjective3);
+      myZooVariable.SetAdjective4(adjective4);
+      myZooVariable.SetVerb1(verb1);
+      myZooVariable.SetVerb2(verb2);
+      myZooVariable.SetVerb3(verb3);
+      myZooVariable.SetAdverb1(adverb1);
+      myZooVariable.SetAdverb2(adverb2);
+      return View(myZooVariable);
+    }
+    [Route("/toy")]
+//    public ActionResult Toy(string picture1, string exclamation1, string sound1, string adjective1, string noun1, string month1, string sound2, string adjective2, string noun2, string verb1, string verb2, string drink1, string number1, string color1, string color2, string number2, string noun3, string noun4)
+    public ActionResult Toy(string exclamation1)
+    {
+      ToyVariable myToyVariable = new ToyVariable();
+      // myToyVariable.SetPicture1(picture1);
+      myToyVariable.SetExclamation1(exclamation1);
+      // myToyVariable.SetSound1(sound1);
+      // myToyVariable.SetAdjective1(adjective1);
+      // myToyVariable.SetNoun1(noun1);
+      // myToyVariable.SetMonth1(month1);
+      // myToyVariable.SetSound2(sound2);
+      // myToyVariable.SetAdjective2(adjective2);
+      // myToyVariable.SetNoun2(noun2);
+      // myToyVariable.SetVerb1(verb1);
+      // myToyVariable.SetVerb2(verb2);
+      // myToyVariable.SetDrink1(drink1);
+      // myToyVariable.SetNumber1(number1);
+      // myToyVariable.SetColor1(color1);
+      // myToyVariable.SetColor2(color2);
+      // myToyVariable.SetNumber2(number2);
+      // myToyVariable.SetNoun3(noun3);
+      // myToyVariable.SetNoun4(noun4);
+      return View(myToyVariable);
+    }
+
+    [Route("/zoo_form")]
+    public ActionResult ZooForm() { return View(); }
+
+    [Route("/toy_form")]
+    public ActionResult ToyForm() { return View(); }
+
+    [Route("/")]
+    public ActionResult Form() { return View(); }
+  }
+}
---
+++ MadLibs/Models/ZooVariable.cs
@@ -0,0 +1,137 @@
+namespace MadLibs.Models
+{
+  public class ZooVariable
+  {
+    private string _adjective1;
+    private string _noun1;
+    private string _verb1;
+    private string _adverb1;
+    private string _adjective2;
+    private string _noun2;
+    private string _noun3;
+    private string _adjective3;
+    private string _verb2;
+    private string _adverb2;
+    private string _verb3;
+    private string _adjective4;
+
+    public string GetAdjective1()
+    {
+      return _adjective1;
+    }
+
+    public void SetAdjective1(string newAdjective)
+    {
+      _adjective1 = newAdjective;
+    }
+
+    public string GetAdjective2()
+    {
+      return _adjective2;
+    }
+
+    public void SetAdjective2(string newAdjective)
+    {
+      _adjective2 = newAdjective;
+    }
+
+    public string GetAdjective3()
+    {
+      return _adjective3;
+    }
+
+    public void SetAdjective3(string newAdjective)
+    {
+      _adjective3 = newAdjective;
+    }
+
+    public string GetAdjective4()
+    {
+      return _adjective4;
+    }
+
+    public void SetAdjective4(string newAdjective)
+    {
+      _adjective4 = newAdjective;
+    }
+
+    public string GetNoun1()
+    {
+      return _noun1;
+    }
+
+    public void SetNoun1(string newNoun)
+    {
+      _noun1 = newNoun;
+    }
+
+    public string GetNoun2()
+    {
+      return _noun2;
+    }
+
+    public void SetNoun2(string newNoun)
+    {
+      _noun2 = newNoun;
+    }
+    public string GetNoun3()
+    {
+      return _noun3;
+    }
+
+    public void SetNoun3(string newNoun)
+    {
+      _noun3 = newNoun;
+    }
+
+    public string GetVerb1()
+    {
+      return _verb1;
+    }
+
+    public void SetVerb1(string newVerb)
+    {
+      _verb1 = newVerb;
+    }
+
+    public string GetVerb2()
+    {
+      return _verb2;
+    }
+
+    public void SetVerb2(string newVerb)
+    {
+      _verb2 = newVerb;
+    }
+    public string GetVerb3()
+    {
+      return _verb3;
+    }
+
+    public void SetVerb3(string newVerb)
+    {
+      _verb3 = newVerb;
+    }
+
+    public string GetAdverb1()
+    {
+      return _adverb1;
+    }
+
+    public void SetAdverb1(string newAdverb)
+    {
+      _adverb1 = newAdverb;
+    }
+
+    public string GetAdverb2()
+    {
+      return _adverb2;
+    }
+
+    public void SetAdverb2(string newAdverb)
+    {
+      _adverb2 = newAdverb;
+    }
+  }
+
+}
